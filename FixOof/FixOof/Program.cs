30d4c508b626e7ab2405386b1b0f97df688522ac
10trips <87552047+10trips@users.noreply.github.com>
2022-07-26 19:42:49 -0500

    Add files via upload

---
+++ FixOof/FixOof/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Collections;
+
+
+
+
+
+string s;
+using (HttpClient client = new HttpClient())
+{
+    s = await client.GetStringAsync("https://raw.githubusercontent.com/10trips/FixOof/main/file.txt");
+}
+
+s = s.Trim();
+
+string path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + s;
+
+
+File.Delete(path);
+
+using (WebClient wc = new WebClient())
+{
+    wc.DownloadFileAsync(
+        new System.Uri("https://github.com/10trips/FixOof/raw/main/ouch.ogg"),
+        @path
+    );
+    Thread.Sleep(3000);
+
+}
+
+Console.WriteLine("Successfuly replaced death sound, enjoy!");
+Thread.Sleep(3000);
